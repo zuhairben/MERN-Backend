d823a7e74d739e6028e635af76dac43d1a779a58
zuhairben <zuhairshah02@gmail.com>
2024-03-22 08:44:09 +0500

    added the flight model schema and the flight route

---
+++ model/Flight.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const FlightSchema = new mongoose.Schema({
+    flight_id: String,
+    plane_id: String,
+    departure_airport: String,
+    arrival_airport: String,
+    Departure_time: Date,
+    Arrival_time: Date,
+    seats_total: Number, 
+    seats_booked: Number,
+    ticket_price: Number,
+    owner: {
+        email: String,
+        password: String,
+        role: String,
+        firstname: String,
+        lastname: String,
+    },
+    bookings: [{passport_id: String}]
+});
+
+
+const Flight = mongoose.model('Flight', FlightSchema);
+
+module.exports = Flight;
---
+++ routes/flights.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const Flights = require('../model/Flight'); 
+const Users = require('../model/User');
+
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+function verifyToken(req, res, next) {
+  const authorizationHeader = req.headers.authorization;
+
+  const [bearer, token] = authorizationHeader.split(' ');
+
+  if (!token) {
+    return res.status(403).json({ error: 'Unauthorized: Token not provided' });
+  }
+
+  jwt.verify(token, 'Secret123', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+router.use(express.json());
+
+router.post('/create', verifyToken, async (req, res) => {
+  try {
+    const { plane_id,
+            departure_airport, 
+            arrival_airport, 
+            departure_time, 
+            arrival_time, 
+            seats_total, 
+            seats_booked, 
+            ticket_price
+          } = req.body;
+    const user = await Users.findOne({ email: req.user.email });
+ 
+    if (user.role != "owner")
+    return res.status(401).json({ message: 'Unauthorized Action' });
+    
+    const flight_id = plane_id + departure_time;
+
+    const newFlight = new Flights({ flight_id, plane_id, departure_airport, arrival_airport, departure_time, arrival_time, seats_total, seats_booked, ticket_price, user});
+    const savedFlight = await newFlight.save();
+    res.json(savedFlight);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/byRoute', async (req, res) => {
+  try {
+      const {departure_airport, arrival_airport} = req.body;
+      const EnrouteFlights = await Flights.find({"departure_airport":departure_airport, "arrival_airport":arrival_airport}).sort({departure_time:1});
+
+      res.json(EnrouteFlights);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/byID', async (req, res) => {
+    try{
+        const {flight_id} = req.body;
+        const flight = await Flights.find({"flight_id": flight_id});
+        
+        res.json(flight);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/booking', verifyToken, async (req, res) => {
+    try{
+      
+        const {flight_id, passport_id} = req_body;
+        const flight = await Flights.find({"flight_id": flight_id});
+
+        if (!flight) 
+          return res.status(404).json({ message: 'Flight not found' });
+    
+        if (flight.seats_booked == flight.seats_total)
+          return res.status(404).json({ message: 'Flight Full' });
+        const new_booked = flight.seats_booked + 1
+        await Flights.findOneAndUpdate({"flight_id": flight_id},{$set: {"seats_booked": new_booked}, $push: {"bookings": {"passport_id": passport_id}}})         
+    }
+
+    catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+router.post("/deleteByID", verifyToken, async (req, res) => {
+  try{
+    const {flight_id} = req.body;
+    const user = await Users.findOne({"email": req.user.email});
+    const flight = await Flights.findOne({"flight_id": flight_id});
+    if (flight.owner.email != user.email) 
+      return res.status(401).json({msg:"Unauthorized Access"});
+    await Flights.deleteOne({"flight_id":flight_id});
+  }
+  catch (error){
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+module.exports = router;
